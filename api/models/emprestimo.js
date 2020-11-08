f52b017d1023dac3a9ff0d3e0a3ec5426f8867d9
Manoel Fernando <manoelfernandoconceicao@gmail.com>
2020-11-08 17:50:43 -0300

    Emprestimo Completo

--- api/models/emprestimo.js
+++ api/models/emprestimo.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    idAluno: {type: Number,required: true},
-    idKit: {type: [String], required: true}, //ver como declarar array e pegar de Kit
+    idAluno: {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
+    idKit: {type: [mongoose.Schema.Types.ObjectId], ref: 'Kit', required: true}, //ver como declarar array e pegar de Kit
     descricao: {type: String},
     ocorrencia: {type: String},
     dtEmprestimo: {type: Date, default: Date.now, required: true}, 
     dtFinalizacaoEmprestimo: {type: Date, default: null},
     status: {type: String, required: true},
-    codigoMonitorEmprestimo: {type: Number, required: true},
+    codigoMonitorEmprestimo: {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
     codigoMonitorFinalizacao: {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario'},
 });
 
