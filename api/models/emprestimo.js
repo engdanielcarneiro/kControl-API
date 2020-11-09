3261098c35a117105ef36bfdcb4b8b4a9b654717
Manoel Fernando <manoelfernandoconceicao@gmail.com>
2020-11-09 17:11:06 -0300

    Heroku

--- api/models/emprestimo.js
+++ api/models/emprestimo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     idAluno: {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
-    idKit: {type: [mongoose.Schema.Types.ObjectId], ref: 'Kit', required: true}, //ver como declarar array e pegar de Kit
+    idKit: {type: [mongoose.Schema.Types.ObjectId], ref: 'Kit', required: true}, 
     descricao: {type: String},
     ocorrencia: {type: String},
     dtEmprestimo: {type: Date, default: Date.now, required: true}, 
