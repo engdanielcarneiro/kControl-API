37a008a9557981b58be57e3ca4a8e958c4a858c6
Daniel <daniel.alexandre97@hotmail.com>
2020-11-09 23:17:25 -0300

    configs: added configs to work with system

--- api/models/emprestimo.js
+++ api/models/emprestimo.js
@@ -4,13 +4,13 @@ const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     idAluno: {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
     idKit: {type: [mongoose.Schema.Types.ObjectId], ref: 'Kit', required: true}, //ver como declarar array e pegar de Kit
-    descricao: {type: String},
-    ocorrencia: {type: String},
+    descricao: {type: String, default: null},   
+    ocorrencia: {type: String, default: null},
     dtEmprestimo: {type: Date, default: Date.now, required: true}, 
     dtFinalizacaoEmprestimo: {type: Date, default: null},
-    status: {type: String, required: true},
+    status: {type: String, default: "Em aberto", required: true},
     codigoMonitorEmprestimo: {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
-    codigoMonitorFinalizacao: {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario'},
+    codigoMonitorFinalizacao: {type: mongoose.Schema.Types.ObjectId, default: null, ref: 'Usuario'},
 });
 
 module.exports = mongoose.model('Emprestimo', productSchema);
